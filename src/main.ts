4f75e44c7ca51eba4b912f10c49761ce5f3f7ce2
tanzhifei <tanzhifei@enn.cn>
2021-10-29 17:32:45 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
 
-createApp(App).use(router).mount('#app')
+let app = createApp(App)
+app.use(ElementPlus)
+app.use(router).mount('#app')
